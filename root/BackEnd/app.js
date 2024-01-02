9af2ddb67eace01d33a93f1fc2868bd97e74a855
Denis Onica <denis.onica97@gmail.com>
2024-01-02 18:49:43 +0200

    added endpoints to the tables used

--- root/BackEnd/app.js
+++ root/BackEnd/app.js
@@ -9,8 +9,32 @@ app.get("/", (req, res) => {
   res.send("test");
 });
 
-app.get("/show", (req, res) => {
+app.get("/table/passwords", (req, res) => {
   let sql = `SELECT * FROM passwords`;
+  mysql.execute(sql, (err, result) => {
+    console.log(result);
+    res.json(result);
+  });
+});
+
+app.get("/table/machine", (req, res) => {
+  const sql = `SELECT * FROM machines`;
+  mysql.execute(sql, (err, result) => {
+    console.log(result);
+    res.json(result);
+  });
+});
+
+app.get("/table/processes", (req, res) => {
+  const sql = `SELECT * FROM processes`;
+  mysql.execute(sql, (err, result) => {
+    console.log(result);
+    res.json(result);
+  });
+});
+
+app.get("/table/applications", (req, res) => {
+  const sql = `SELECT * FROM applications`;
   mysql.execute(sql, (err, result) => {
     console.log(result);
     res.json(result);
